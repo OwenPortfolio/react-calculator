45e5b2bb4c0519275d83a66c8cf3f2c18dcde7d8
OwenPortfolio <owen.manby@gmail.com>
2022-08-01 16:37:00 +0100

    add basic styling and readme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ const Header = () => {
     return (
         <>
         <h1>Owen's Calculator</h1>
+        <h3>This is an ongoing project to make a calculator in react.</h3>
         </>
     )
 }
