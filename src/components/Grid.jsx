553731e19d3710fa8f710fe34adfcdcf983ba13e
OwenPortfolio <owen.manby@gmail.com>
2022-08-02 10:47:00 +0100

    complete styling to resemble real-life calculator

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -31,30 +31,31 @@ const Grid = () => {
     }
     return (
         <>
-        <TextBox text={text}/>
-        <div id='pageContainer'>
-        <div id='ButtonGrid'>
-            <button className='numberButton' onClick={buttonPress}>7</button>
-            <button className='numberButton' onClick={buttonPress}>8</button>
-            <button className='numberButton' onClick={buttonPress}>9</button>
-            <button className='numberButton' onClick={buttonPress}>4</button>
-            <button className='numberButton' onClick={buttonPress}>5</button>
-            <button className='numberButton' onClick={buttonPress}>6</button>
-            <button className='numberButton' onClick={buttonPress}>1</button>
-            <button className='numberButton' onClick={buttonPress}>2</button>
-            <button className='numberButton' onClick={buttonPress}>3</button>
-            <button className='numberButton' onClick={buttonPress}>0</button>
+        <div id='Calculator'>
+            <TextBox text={text}/>
+            <div id='pageContainer'>
+            <div id='ButtonGrid'>
+                <button className='numberButton' onClick={buttonPress}>7</button>
+                <button className='numberButton' onClick={buttonPress}>8</button>
+                <button className='numberButton' onClick={buttonPress}>9</button>
+                <button className='numberButton' onClick={buttonPress}>4</button>
+                <button className='numberButton' onClick={buttonPress}>5</button>
+                <button className='numberButton' onClick={buttonPress}>6</button>
+                <button className='numberButton' onClick={buttonPress}>1</button>
+                <button className='numberButton' onClick={buttonPress}>2</button>
+                <button className='numberButton' onClick={buttonPress}>3</button>
+                <button className='numberButton' onClick={buttonPress}>0</button>
+            </div>
+            <div id='ActionGrid'>
+                <button className='actionButton' onClick={buttonPress}>+</button>
+                <button className='actionButton' onClick={buttonPress}>-</button>
+                <button className='actionButton' onClick={buttonPress}>*</button>
+                <button className='actionButton' onClick={buttonPress}>/</button>
+                <button className='actionButton' onClick={equals}>=</button>
+                <button className='actionButton' onClick={clear}>C</button>
+            </div>
+            </div>
         </div>
-        <div id='ActionGrid'>
-            <button className='actionButton' onClick={buttonPress}>+</button>
-            <button className='actionButton' onClick={buttonPress}>-</button>
-            <button className='actionButton' onClick={buttonPress}>*</button>
-            <button className='actionButton' onClick={buttonPress}>/</button>
-            <button className='actionButton' onClick={equals}>=</button>
-            <button className='actionButton' onClick={clear}>C</button>
-        </div>
-        </div>
-
         </>
 
     )
